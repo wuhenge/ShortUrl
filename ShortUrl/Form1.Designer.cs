7b1f89bb173bc650176092d2c293535515432ffb
Healer <xiaoheiwangluo@live.com>
2019-08-31 18:49:45 +0800

    短网址生成

--- ShortUrl/Form1.Designer.cs
+++ ShortUrl/Form1.Designer.cs
@@ -28,13 +28,226 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(338, 65);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "短网址类型";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(11, 28);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(94, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "新浪（t.cn）";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(111, 28);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(105, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "腾讯（url.cn）";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(222, 28);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(113, 21);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "百度（dwz.cn）";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.linkLabel1);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 85);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(338, 80);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "短网址配置";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "百度Token：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(94, 53);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(87, 21);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "记住Token";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(256, 54);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(68, 17);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "获取Token";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(12, 171);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(338, 135);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "短网址生成";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(94, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(230, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "原网址：";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(94, 61);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(230, 23);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "短网址：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "生 成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(364, 321);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "短网址生成";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ShortUrl/Form1.cs
+++ ShortUrl/Form1.cs
@@ -1,19 +1,154 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using Newtonsoft.Json.Linq;
+using SufeiUtil;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Web;
 using System.Windows.Forms;
 
 namespace ShortUrl
 {
     public partial class Form1 : Form
     {
+        RegistryKey regkey;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            regkey = Registry.CurrentUser.OpenSubKey(@"Software\ShortUrl");
+            if (regkey != null)
+            {
+                var BaiduTokenValue = regkey.GetValue("BaiduTokenValue");
+                if (BaiduTokenValue != null)
+                {
+                    textBox1.Text = BaiduTokenValue.ToString();
+                    if (!string.IsNullOrEmpty(textBox1.Text))
+                        checkBox1.Checked = true;
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            regkey = Registry.CurrentUser.CreateSubKey(@"Software\ShortUrl");
+            var flag = checkBox1.Checked;
+            if (flag)
+                regkey.SetValue("BaiduTokenValue", textBox1.Text.Trim());
+            else
+                regkey.SetValue("BaiduTokenValue", string.Empty);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://dwz.cn/console/userinfo");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var type = "t.cn";
+                if (radioButton1.Checked) type = "t.cn";
+                else if (radioButton2.Checked) type = "url.cn";
+                else if (radioButton3.Checked) type = "dwz.cn";
+                var yUrl = textBox2.Text.Trim();
+                var dUrl = textBox3.Text.Trim();
+                var baiduToken = textBox1.Text.Trim();
+                if (string.IsNullOrEmpty(yUrl) || !yUrl.StartsWith("http")) return;
+                string url = "", html = "", result = "";
+                if (type == "t.cn")
+                {
+                    url = "http://api.t.sina.com.cn/short_url/shorten.json?source=3271760578&url_long=" + HttpUtility.UrlEncode(yUrl);
+                    html = GetHttpContent(url);
+                    if (html.StartsWith("["))
+                    {
+                        var json = JArray.Parse(html);
+                        result = json.First["url_short"].ToString();
+                    }
+                }
+                else if (type == "url.cn")
+                {
+                    url = "http://sa.sogou.com/gettiny?url=" + HttpUtility.UrlEncode(yUrl);
+                    html = GetHttpContent(url);
+                    result = html;
+                }
+                else if (type == "dwz.cn")
+                {
+                    if (string.IsNullOrEmpty(baiduToken))
+                    {
+                        MessageBox.Show("百度Token不可为空"); return;
+                    }
+                    else
+                    {
+                        url = "https://dwz.cn/admin/v2/create";
+                        string postData = "{\"url\":\"" + yUrl + "\",\"TermOfValidity\":\"long-term\"}";
+                        html = GetHttpContent(url, "post", postData, new Dictionary<string, string> { { "Token", baiduToken } });
+                        var json = JObject.Parse(html);
+                        string code = json["Code"].ToString();
+                        if (code == "0")
+                        {
+                            string ShortUrl = json["ShortUrl"].ToString();
+                            result = ShortUrl;
+                        }
+                        else
+                        {
+                            string ErrMsg = json["ErrMsg"].ToString();
+                            result = ErrMsg;
+                        }
+                    }
+                }
+                textBox3.Text = result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 发送HTTP请求并获取返回数据
+        /// </summary>
+        /// <param name="url">URL</param>
+        /// <param name="method">HTTP请求类型，GET或者POST</param>
+        /// <param name="postdata">POST请求数据</param>
+        /// <param name="headers">HTTP请求头</param>
+        /// <returns></returns>
+        public string GetHttpContent(string url, string method = "get", string postdata = "", Dictionary<string, string> headers = null)
+        {
+            HttpHelper http = new HttpHelper();
+            HttpItem item = new HttpItem()
+            {
+                URL = url,//URL     必需项
+                Encoding = null,//编码格式（utf-8,gb2312,gbk）     可选项 默认类会自动识别
+                Method = method,//URL     可选项 默认为Get
+                Timeout = 100000,//连接超时时间     可选项默认为100000
+                ReadWriteTimeout = 30000,//写入Post数据超时时间     可选项默认为30000
+                UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36",//用户的浏览器类型，版本，操作系统     可选项有默认值
+                Accept = "text/html, application/xhtml+xml, */*",//    可选项有默认值
+                ContentType = "text/html",//返回类型    可选项有默认值
+                Postdata = postdata,//Post数据     可选项GET时不需要写
+            };
+            if (headers != null)
+            {
+                foreach (var k in headers)
+                {
+                    item.Header.Add(k.Key, k.Value);
+                }
+            }
+            //得到HTML代码
+            HttpResult result = http.GetHtml(item);
+            //返回的Html内容
+            return result.Html;
+        }
+
     }
 }
